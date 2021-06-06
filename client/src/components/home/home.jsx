f6b944ffc687a28085737824b0fc89fe00e4940b
afif ahmed <afifahmed456123@gmail.com>
2021-06-06 16:47:41 +0530

    refactor: removed the ul from the home page

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,20 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const Home = () => {
-    return (
-        <div>
-            <ul>
-                <li>
-                    <Link to="/" >Home</Link>
-                </li>
-                <li>
-                    <Link to="/createImage">Create Image</Link>
-                </li>
-                <li>
-                    <Link to="/systems">Systems Manager</Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
+	return <div>This is the home page</div>;
+};
